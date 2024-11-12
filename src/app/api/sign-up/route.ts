43e45ce5cb15967ecaf4bb45d80d9940eda7efc6
Umar Farooq <umar.farooq@devigital.com>
2024-11-12 19:17:41 +0500

    User signup and verification email template

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,92 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel, { User } from "@/model/User";
+import bcrypt from 'bcryptjs'
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const {email, password, username } = await request.json();
+        const existingUserVerifiedByUsername = await UserModel.find({username, isVerified: true})
+        if(existingUserVerifiedByUsername){
+            return new Response(
+                JSON.stringify({
+                        success: false,
+                        message: 'Username already taken',
+                    }),
+                    { status: 400 }
+              );
+        }
+        const existingUserByEmail = await UserModel.findOne({email})
+        let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+        if(existingUserByEmail){
+            if(existingUserByEmail.isVerified){
+                return new Response(
+                    JSON.stringify({
+                            success: false,
+                            message: 'User already exists with this email',
+                        }),
+                        { status: 400 }
+                  ); 
+            }else{
+                const hashPassword = await bcrypt.hash(password, 10);
+                existingUserByEmail.password = hashPassword;
+                existingUserByEmail.verifyCode = verifyCode;
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000),
+                await existingUserByEmail.save();
+            }
+        }else{
+            const hashPassword = await bcrypt.hash(password, 10);
+            // const expiryDate = new Date()
+            // expiryDate.setHours(expiryDate.getHours() + 1) //can use this too
+            let newUser = new UserModel({
+                username,
+                email,
+                password,
+                verifyCode,
+                verifyCodeExpiry: new Date(Date.now() + 3600000),
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+
+            await newUser.save()
+
+
+        }
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode
+        )
+        if(!emailResponse.success){
+            return new Response(
+                JSON.stringify({
+                        success: false,
+                        message: emailResponse.message,
+                    }),
+                    { status: 500 }
+              );
+        }
+        return new Response(
+            JSON.stringify({
+                    success: true,
+                    message: "User registered successfully. Please verify your email",
+                }),
+                { status: 200 }
+          );
+
+
+    } catch (error) {
+        console.error("Error registering user", error)
+        return new Response(
+            JSON.stringify({
+                    success: false,
+                    message: 'Username is already taken',
+                }),
+                { status: 400 }
+          );
+    }
+}
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import { resend } from "@/lib/resend";
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+export const sendVerificationEmail = async (
+    email : string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> => {
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev', //your domain
+            to: email,
+            subject: 'Whisper Wire | Verification Code',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+        return {success: true, message: "Verification email sent successfully!"}
+          
+    } catch (error) {
+        return {success: false, message: "Failed to send verification email!"}
+    }
+}
